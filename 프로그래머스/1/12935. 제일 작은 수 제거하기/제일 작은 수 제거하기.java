0b286aaf632f4f4ea15e4cad5f0fdafb9d340b73
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2024-08-14 09:21:12 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.37 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,35 @@
+// 제일 작은 수 제거하기
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        int min = arr[0];
+        int minIndex = 0;
+        
+        if(arr.length ==1){
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int[] answer = new int[arr.length -1];
+        
+        // 작은 수의 인덱스 값 구하기
+        for(int i =0 ; i< arr.length; i++){
+            if(min>arr[i]){
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        
+        // 작은 수를 제외한 나머지 값 배열에 넣어주기
+        int index = 0;
+        for(int i =0 ;i< arr.length; i++){
+            if(i == minIndex){
+                continue;
+            }else{
+                answer[index++]=arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
