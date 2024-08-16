0887ea7342821f0d6b5321deb5283b7c1f0fed5a
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2024-08-16 10:54:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,17 @@
+//핸드폰 번호 가리기
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        char[] charPhoneNumber = phone_number.toCharArray();
+        int length = charPhoneNumber.length;
+        for(int i=0;i<length;i++){
+            if(i<length-4){
+                charPhoneNumber[i]='*';
+            }
+        }
+        answer = String.valueOf(charPhoneNumber);
+        //answer = charPhoneNumber;
+        return answer;
+    }
+}
