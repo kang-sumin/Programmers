27091f02d5f90cc1e6887baccb0285b9a1fbc629
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2024-08-22 11:21:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.16 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,22 +1,16 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        char[] str = s.toCharArray();
         
-        // 문자열 길이는 4 or 6 
-        if(str.length == 4 || str.length ==6){
-            for(int i = 0 ; i < str.length ; i++){
-                if(48 <= str[i] && str[i] <= 57){
-                    answer = true;
-                }else{
-                    answer = false;
-                    break;
-                }
+        if(s.length() == 4 || s.length() == 6){
+            if(s.matches("(^[0-9]*$)")){
+                answer = true;
+            }else{
+                answer = false;
             }
         }else{
             answer = false;
         }
-        
         return answer;
     }
 }
