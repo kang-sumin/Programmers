922b51d178f1a337b6896f3258ed836d1a607486
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2024-12-20 15:35:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.24 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -7,11 +7,13 @@ class Solution {
         
         Stack<Integer> stack = new Stack<>();
         
+        // 3진법으로 변환하여 stack 에 저장
         while(n != 0 ){
             stack.push(n%3);
             n/=3;
         }
         
+        // Stack 자료구조의 LIFO(Last In First Out) 구조사용해 10진법으로 변환 
         while(!stack.isEmpty()){
             answer += stack.pop() * Math.pow(3,count);
             count++;
